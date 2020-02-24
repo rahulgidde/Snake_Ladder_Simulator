fb3158813291b72d35462f69108f5f94733e692c
rahulgidde <rahulgidde1212@gmail.com>
2020-02-24 16:17:50 +0530

    ADDED:Roll Die For Player

--- SnakeLadderSimulator.sh
+++ SnakeLadderSimulator.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "--------------Snake & Ladder Simulator--------------"
 
 #CONSTANTS
 NUMBER_OF_PLAYER=1
 START_POSITION=0
+
+#ROLL DIE FOR PLAYER
+rollDie=$((1 + RANDOM%6))
