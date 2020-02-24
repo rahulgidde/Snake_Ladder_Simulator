2ab3760262c032f4dd6df923c4f25e8699949f0a
rahulgidde <rahulgidde1212@gmail.com>
2020-02-24 17:11:20 +0530

    ADDED:Check Option For No Player Ladder or Snake

--- SnakeLadderSimulator.sh
+++ SnakeLadderSimulator.sh
@@ -5,6 +5,33 @@ echo "--------------Snake & Ladder Simulator--------------"
 #CONSTANTS
 NUMBER_OF_PLAYER=1
 START_POSITION=0
+WIN=100
+
+#VARIABLE
+position=$START_POSITION
 
 #ROLL DIE FOR PLAYER
-rollDie=$((1 + RANDOM%6))
+function rollDie()
+{
+	echo $((1 + RANDOM%6))
+}
+
+
+#CHECK FOR OPTION
+checkCase=$((1+RANDOM%3))
+
+case $checkCase in
+	1)
+		position=$(( position + rollDie ))
+		echo "Player moves ahead by $rollDie position."
+		;;
+	2)
+		position=$(( position - rollDie ))
+		echo "Player moves behind by $rollDie position."
+		;;
+	3)
+		echo "Player is on same Position."
+		;;
+esac
+
+
